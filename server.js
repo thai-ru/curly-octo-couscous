964f04736fd5c3287a5868ea3943ec2a7928f6ae
thai-ru <robinthairu34@gmail.com>
2023-05-22 21:59:16 +0300

    Keys

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/users', (req, res) => {
       });
   });
 
-    const PORT = 3000; // Change the port number as needed
+    const PORT = 8000; // Change the port number as needed
 
     app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
